1dd034a174ebe05b6d8b39ecc8a4cd6ae4e43a11
YingxinLin <yingxinlindsay@gmail.com>
2021-07-06 14:18:57 +1000

    fix joint prediction

--- R/ensemble.R
+++ R/ensemble.R
@@ -31,35 +31,36 @@ getEnsembleRes <- function(testRes, weighted_train,
                                   names(weighted_train))
             weighted_train <- weighted_train[keep_method]
         }
-
+        
     } else {
         weighted_train <- NULL
     }
-
-    ensembleRes <-   calEnsembleRes(testRes,
-                                    exclude = exclude,
-                                    weight = weighted_train)
+    
+    ensembleRes <- calEnsembleRes(testRes,
+                                  exclude = exclude,
+                                  weight = weighted_train)
     return(ensembleRes)
-
+    
 }
 
 # Function to combine the ensemble res
 
 calEnsembleRes <- function(res, exclude = NULL, weight = NULL){
-
-
+    
+    
     if (!is.null(exclude)) {
         keep_method <- !grepl(paste(exclude,collapse = "|"), names(res))
         res <- res[keep_method]
         weight <- weight[keep_method]
     }
-
-    if (is.null(weight)) {
-        weight <- rep(1, length(res))
-    }
+    
 
     ensembleResMat <- do.call(cbind, lapply(res, function(x) x$predRes))
-
+    
+    if (is.null(weight)) {
+        weight <- rep(1, ncol(ensembleResMat))
+    }
+    
     ensembleRes <- apply(ensembleResMat, 1, function(x) {
         names(x) <- colnames(ensembleResMat)
         keep <- rep(TRUE, length(x))
@@ -68,9 +69,9 @@ calEnsembleRes <- function(res, exclude = NULL, weight = NULL){
         }else{
             getResByWeights(x[keep], weight[keep])
         }
-
+        
     })
-
+    
     ensembleRes <- do.call(rbind, ensembleRes)
     return(ensembleRes)
 }
@@ -86,19 +87,19 @@ getResByWeights <- function(res, weight) {
         final <- resType
         scores <- 1
     } else {
-
-
+        
+        
         mat <- vapply(seq_len(length(resType)), function(i){
             ifelse(res %in% resType[i], 1, 0) * weight
         }, numeric(length(res)))
-
+        
         colnames(mat) <- resType
         rownames(mat) <- names(res)
         mat_colMeans <- Matrix::colMeans(mat)
         final <- names(mat_colMeans)[which.max(mat_colMeans)]
         scores <- max(mat_colMeans)/sum(mat_colMeans)
     }
-
+    
     return(data.frame(cellTypes = final, scores = scores))
 }
 
@@ -108,41 +109,41 @@ getResByWeights <- function(res, weight) {
 # Function to summarise joint classification results
 
 getJointRes <- function(predictResList, trainRes) {
-
+    
     predictJointResList <- lapply(seq_along(predictResList), function(l) {
-
+        
         if ("scClassifyTrainModel" %in% is(trainRes[[l]])) {
             weights_train <- modelweights(trainRes[[l]])
         } else {
             weights_train <- trainRes[[l]]$modelweights
         }
-
+        
         getEnsembleRes(predictResList[[l]],
                        weights_train,
                        exclude = NULL,
                        weighted_ensemble = TRUE)
     })
-
+    
     names(predictJointResList) <- names(predictResList)
-
-
+    
+    
     jointCellType <- do.call(cbind, lapply(predictJointResList, function(x)
         x$cellTypes))
     colnames(jointCellType) <- names(predictJointResList)
-
-
+    
+    
     jointCellType <- apply(jointCellType, 2, function(x) {
         len <- unlist(lapply(strsplit(x, "_"), length))
         x[len > 2] <- "intermediate"
         x
     })
-
+    
     jointWeights <- do.call(cbind, lapply(predictJointResList, function(x)
         x$scores))
     colnames(jointWeights) <- names(predictJointResList)
-
-
-
+    
+    
+    
     jointRes <- lapply(seq_len(nrow(jointCellType)), function(idx) {
         cell_res <- jointCellType[idx, ]
         names(cell_res) <- colnames(jointCellType)
@@ -153,12 +154,12 @@ getJointRes <- function(predictResList, trainRes) {
             getResByWeights(cell_res, jointWeights[idx, ])
         }
     })
-
-
+    
+    
     jointRes <- do.call(rbind, jointRes)
-
+    
     jointRes <- data.frame(jointRes)
-
+    
     return(jointRes)
 }
 
